2d5cafc0c922a981bb78956b74974c6b149e4866
Your Nameazkamalaz <kamalazenag@gmail.com>
2023-12-10 11:33:41 -0800

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,10 @@
- #!/usr/bin/python3
-"""Defines the Amenity class."""
+#!/usr/bin/python3
+"""This module creates a Amenity class"""
+
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Represent an amenity.
-
-    Attributes:
-        name (str): The name of the amenity.
-    """
+    """Class for managing amenity objects"""
 
     name = ""
